e3d8d77ba4d0df54f7eadd78a9c79c780a1e524b
orlb <brownolb2@gmail.com>
2022-09-16 09:45:34 -0800

    added cursor

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main() {
 	
 	int screen[128][128];
 	int origin[2] = {35, 0};
-<<<<<<< HEAD
 
 	int live[][2] = {
 		{10, 10},
@@ -24,16 +23,6 @@ int main() {
 
 	/*int live[][2] = {	// glider gun
 		{5, 1},
-=======
-	int live[][2] = {	
-        
-        {10, 10},
-        {11, 11},
-        {11, 12},
-        {11, 13},
-        {10, 14}
-/*		{5, 1},     // glider gun
->>>>>>> a4d53b68f22d050b8ed28b08e7ae9cb78a999f11
 		{5, 2},
 		{6, 1},
 		{6, 2},
@@ -69,12 +58,7 @@ int main() {
 		{4, 35},
 		{3, 36},
 		{4, 36}
-<<<<<<< HEAD
 	};*/
-=======
-*/
-	};
->>>>>>> a4d53b68f22d050b8ed28b08e7ae9cb78a999f11
 
 	for (int y = 0; y < SIZE; y++) { 	// screen init
 		for (int x = 0; x < SIZE; x++) {
